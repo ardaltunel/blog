64feecf7d34063628131a520db750e094a2be9ac
Arda Altunel <ardaltunelmain@gmail.com>
2026-09-08 01:57:39 +0300

    Remove footer divider and refresh cached page assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const POSTS_PER_PAGE = 9;
-    const PAGINATION_CACHE_VERSION = '77';
+    const PAGINATION_CACHE_VERSION = '78';
     const MAX_CATEGORIES = 500;
     const MAX_AUTHORS = 2000;
     const MAX_POSTS = 2000;
