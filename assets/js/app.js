e9ce2ebe6b1188b2337fbae2718096f0b39ea2a2
Arda Altunel <ardaltunelmain@gmail.com>
2026-09-11 02:38:54 +0300

    Match category heading spacing to blog pages

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     const POSTS_PER_PAGE = 9;
-    const PAGINATION_CACHE_VERSION = '79';
+    const PAGINATION_CACHE_VERSION = '80';
     const MAX_CATEGORIES = 500;
     const MAX_AUTHORS = 2000;
     const MAX_POSTS = 2000;
